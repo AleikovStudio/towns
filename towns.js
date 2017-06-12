2044b7f4c46dd47a6f5cbd4d87b6401b1b3409a2
Aleikov Studio <golearnweb@gmail.com>
2017-06-12 17:13:55 +0300

    Maria's changes

--- towns.js
+++ towns.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 	$('#btnDelete').click(deleteTown);
-	$('#btnAdd').click(addTown);
 });
 
 function deleteTown() {
@@ -14,14 +13,14 @@ function deleteTown() {
 		}
 	}
 	if (removed)
-		$('#result').text(townName + " deleted.");
+		showMessage(townName + " deleted.");
 	else
-		$('#result').text(townName + " not found.");
+		showMessage(townName + " not found.");
 }
 
-function addTown() {
-	let townName = $('#townNameForAdd').val();
-	$('#townNameForAdd').val('');
-	$('#towns').append($('<option>').text(townName));
-	$('#result').text(townName + " added.");
+function showMessage(msg) {
+	$('#result').text(msg).css("display", "block");
+	setTimeout(function() {
+		$('#result').hide('blind', {}, 500);		
+	}, 3000);	
 }
